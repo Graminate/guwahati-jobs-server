a2ee0e29df1e77b1764302d347d95ff7e083e608
Borneel B. Phukan <borneelphukan@gmail.com>
2025-04-25 13:37:12 +0200

    fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,17 @@
 import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-});
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
 export default pool;
